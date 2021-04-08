b3bd9ea9b5f2bfdc2abab5349eb6ed753f17708a
Madihander <79453431+Madihander@users.noreply.github.com>
2021-04-08 22:23:47 +0600

    Update record_to_doctor.js

--- js/record_to_doctor.js
+++ js/record_to_doctor.js
@@ -1,6 +1,11 @@
-let img_add = $("#img_add_path");
+let form = $('#form');
 $(function () {
-    $("#datapicker").datepicker({
+    $('#tbody').sortable({
+        connectWith: ".connectedSortable"
+    }).disableSelection();
+});
+$(function () {
+    $("#minDatepicker").datepicker({
         showOn: "button",
         buttonImage: "https://snipp.ru/demo/437/calendar.gif",
         buttonImageOnly: true,
@@ -8,7 +13,7 @@ $(function () {
     });
 });
 $(function () {
-    $("#phoneNumber").mask("8(999) 999-9999")
+    $("#phoneNumber").mask("+7(999)999-99-99")
 });
 $(function () {
     $("#datepicker").datepicker({
@@ -181,6 +186,323 @@ function writeMonth(Month) {
     $("#month_id").text(Month);
 }
 
+function hideOption() {
+    let time = $('#startTimeHour').val();
+    if (time === '09') {
+        $('#endHour10').removeAttr("hidden");
+        $('#endHour11').removeAttr("hidden");
+        $('#endHour12').removeAttr("hidden");
+        $('#endHour13').removeAttr("hidden");
+        $('#endHour14').removeAttr("hidden");
+        $('#endHour15').removeAttr("hidden");
+        $('#endHour16').removeAttr("hidden");
+        $('#endHour17').removeAttr("hidden");
+        $('#endHour18').removeAttr("hidden");
+        $('#endHour19').removeAttr("hidden");
+    }
+    if (time === '10') {
+        $('#endHour09').attr("hidden", "true");
+        $('#endHour10').removeAttr("hidden");
+        $('#endHour11').removeAttr("hidden");
+        $('#endHour12').removeAttr("hidden");
+        $('#endHour13').removeAttr("hidden");
+        $('#endHour14').removeAttr("hidden");
+        $('#endHour15').removeAttr("hidden");
+        $('#endHour16').removeAttr("hidden");
+        $('#endHour17').removeAttr("hidden");
+        $('#endHour18').removeAttr("hidden");
+        $('#endHour19').removeAttr("hidden");
+    }
+    if (time === '11') {
+        $('#endHour09').attr("hidden", "true");
+        $('#endHour10').attr("hidden", "true");
+        $('#endHour11').removeAttr("hidden");
+        $('#endHour12').removeAttr("hidden");
+        $('#endHour13').removeAttr("hidden");
+        $('#endHour14').removeAttr("hidden");
+        $('#endHour15').removeAttr("hidden");
+        $('#endHour16').removeAttr("hidden");
+        $('#endHour17').removeAttr("hidden");
+        $('#endHour18').removeAttr("hidden");
+        $('#endHour19').removeAttr("hidden");
+    }
+    if (time === '12') {
+        $('#endHour09').attr("hidden", "true");
+        $('#endHour10').attr("hidden", "true");
+        $('#endHour11').attr("hidden", "true");
+        $('#endHour12').removeAttr("hidden");
+        $('#endHour13').removeAttr("hidden");
+        $('#endHour14').removeAttr("hidden");
+        $('#endHour15').removeAttr("hidden");
+        $('#endHour16').removeAttr("hidden");
+        $('#endHour17').removeAttr("hidden");
+        $('#endHour18').removeAttr("hidden");
+        $('#endHour19').removeAttr("hidden");
+
+    }
+    if (time === '13') {
+        $('#endHour09').attr("hidden", "true");
+        $('#endHour10').attr("hidden", "true");
+        $('#endHour11').attr("hidden", "true");
+        $('#endHour12').attr("hidden", "true");
+        $('#endHour13').removeAttr("hidden");
+        $('#endHour14').removeAttr("hidden");
+        $('#endHour15').removeAttr("hidden");
+        $('#endHour16').removeAttr("hidden");
+        $('#endHour17').removeAttr("hidden");
+        $('#endHour18').removeAttr("hidden");
+        $('#endHour19').removeAttr("hidden");
+    }
+    if (time === '14') {
+        $('#endHour09').attr("hidden", "true");
+        $('#endHour10').attr("hidden", "true");
+        $('#endHour11').attr("hidden", "true");
+        $('#endHour12').attr("hidden", "true");
+        $('#endHour13').attr("hidden", "true");
+        $('#endHour14').removeAttr("hidden");
+        $('#endHour15').removeAttr("hidden");
+        $('#endHour16').removeAttr("hidden");
+        $('#endHour17').removeAttr("hidden");
+        $('#endHour18').removeAttr("hidden");
+        $('#endHour19').removeAttr("hidden");
+    }
+    if (time === '15') {
+        $('#endHour09').attr("hidden", "true");
+        $('#endHour10').attr("hidden", "true");
+        $('#endHour11').attr("hidden", "true");
+        $('#endHour12').attr("hidden", "true");
+        $('#endHour13').attr("hidden", "true");
+        $('#endHour14').attr("hidden", "true");
+        $('#endHour15').removeAttr("hidden");
+        $('#endHour16').removeAttr("hidden");
+        $('#endHour17').removeAttr("hidden");
+        $('#endHour18').removeAttr("hidden");
+        $('#endHour19').removeAttr("hidden");
+    }
+    if (time === '16') {
+        $('#endHour09').attr("hidden", "true");
+        $('#endHour10').attr("hidden", "true");
+        $('#endHour11').attr("hidden", "true");
+        $('#endHour12').attr("hidden", "true");
+        $('#endHour13').attr("hidden", "true");
+        $('#endHour14').attr("hidden", "true");
+        $('#endHour15').attr("hidden", "true");
+        $('#endHour16').removeAttr("hidden");
+        $('#endHour17').removeAttr("hidden");
+        $('#endHour18').removeAttr("hidden");
+        $('#endHour19').removeAttr("hidden");
+    }
+    if (time === '17') {
+        $('#endHour09').attr("hidden", "true");
+        $('#endHour10').attr("hidden", "true");
+        $('#endHour11').attr("hidden", "true");
+        $('#endHour12').attr("hidden", "true");
+        $('#endHour13').attr("hidden", "true");
+        $('#endHour14').attr("hidden", "true");
+        $('#endHour15').attr("hidden", "true");
+        $('#endHour16').attr("hidden", "true");
+        $('#endHour17').removeAttr("hidden");
+        $('#endHour18').removeAttr("hidden");
+        $('#endHour19').removeAttr("hidden");
+    }
+    if (time === '18') {
+        $('#endHour09').attr("hidden", "true");
+        $('#endHour10').attr("hidden", "true");
+        $('#endHour11').attr("hidden", "true");
+        $('#endHour12').attr("hidden", "true");
+        $('#endHour13').attr("hidden", "true");
+        $('#endHour14').attr("hidden", "true");
+        $('#endHour15').attr("hidden", "true");
+        $('#endHour16').attr("hidden", "true");
+        $('#endHour17').attr("hidden", "true");
+        $('#endHour18').removeAttr("hidden");
+        $('#endHour19').removeAttr("hidden");
+    }
+    if (time === '19') {
+        $('#endHour09').attr("hidden", "true");
+        $('#endHour10').attr("hidden", "true");
+        $('#endHour11').attr("hidden", "true");
+        $('#endHour12').attr("hidden", "true");
+        $('#endHour13').attr("hidden", "true");
+        $('#endHour14').attr("hidden", "true");
+        $('#endHour15').attr("hidden", "true");
+        $('#endHour16').attr("hidden", "true");
+        $('#endHour17').attr("hidden", "true");
+        $('#endHour18').attr("hidden", "true");
+        $('#endHour19').removeAttr("hidden");
+    }
+}
+
+function formSend(e) {
+    recordPatient();
+    /*let error = formValidate(form)
+    if(error === 0 ){
+        alert('SUCCESS');
+        recordPatient();
+    }else {
+        alert('DANGER');
+    }*/
+}
+
+function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('._req');
+
+    for (let index = 0; index < formReq.length; index++) {
+        const input = formReq[index];
+        formRemoveError(input);
+
+        if (input.classList.contains('_fullName')) {
+            if (fullNameTest(input) === false) {
+                alert(123456)
+                formAddError(input);
+                error++;
+            }
+        } else if (input.classList.contains('_phoneNumber')) {
+            if (phoneNumberTest(input) === false) {
+                formAddError(input);
+                error++;
+            }
+        } else if (input.classList.contains('_minDatepicker')) {
+            if (minDatepickerTest(input) === false) {
+                formAddError(input);
+                error++;
+            }
+        } else {
+            if (input.value === '') {
+                formAddError(input);
+                error++;
+            }
+        }
+    }
+    return error;
+}
+
+function fullNameTest(input) {
+    return (/^([А-ЯA-Z]|[А-ЯA-Z][\x27а-яa-z]{1,}|[А-ЯA-Z][\x27а-яa-z]{1,}-([А-ЯA-Z][\x27а-яa-z]{1,}|(оглы)|(кызы)))\040[А-ЯA-Z][\x27а-яa-z]{1,}(\040[А-ЯA-Z][\x27а-яa-z]{1,})?$/.test(input.value));
+}
+
+function phoneNumberTest(input) {
+    return /^(\+)?(\(\d{2,3}\) ?\d|\d)(([ \-]?\d)|( ?\(\d{2,3}\) ?)){5,12}\d$/.test(input.value);
+}
+
+function minDatepickerTest(input) {
+    return /^(0?[1-9]|[12][0-9]|3[01])[.](0?[1-9]|1[012])[.]\d{4}$/.test(input.value)
+}
+
+function formRemoveError(input) {
+    input.classList.remove('_error')
+}
+
+function formAddError(input) {
+    input.classList.add('_error')
+}
+
+
+function recordPatient() {
+    let patientName = $('#fullName').val();
+    let patientLament = $('#lament').val();
+    let patientNumber = $('#phoneNumber').val();
+    let patientDateRecord = $('#minDatepicker').val();
+    let patientDoctor = $('#doctor').val();
+    let startTime = $('#startTimeHour').val() + ':' + $('#startTimeMin').val();
+    let endTime = $('#endTimeHour').val() + ':' + $('#endTimeMin').val();
+    console.log(patientNumber);
+    $.ajax({
+        url: '../php/record_to_doctor/insert_data_record_patient.php',
+        type: 'POST',
+        data: {
+            name: patientName,
+            lament: patientLament,
+            number: patientNumber,
+            dateRecord: patientDateRecord,
+            doctor: patientDoctor,
+            startTime: startTime,
+            endTime: endTime,
+        },
+        dataType: 'JSON',
+    });
+
+    if (patientDateRecord === $('#datepicker').val()) {
+        let tbody = $('#tbody');
+        let tr = document.createElement('tr');
+        let record_id = 'record_id_'+startTime;
+        tr.innerHTML = `<th scope="time row">
+       <button onclick="showRecordedCard('` + startTime + `','` + patientDateRecord+`','`+record_id+`')">` + startTime + '-' + endTime + '-' + patientDateRecord + `</button>
+</th>
+    <td>` + patientName + ' ,' + patientLament + `</td>`;
+        tr.setAttribute('class', 'ui-state-default');
+        tr.setAttribute('id', record_id);
+        tbody.append(tr);
+
+        $('#recordingCard').modal("hide");
+        $('.data_patient').val('');
+    } else {
+        alert('Пациент записан на ' + patientDateRecord);
+        $('#recordingCard').modal("hide");
+        $('.data_patient').val('');
+    }
+}
+
+function showRecordedCard(time, date,record_id) {
+    alert(134567);
+    alert(record_id);
+    $.ajax({
+        url: '../php/record_to_doctor/showRecordedCard.php',
+        method: 'POST',
+        data: {
+            time: time,
+            date: date,
+        },
+        dataType: 'JSON',
+        success: function (data) {
+            console.log(data);
+            $('#recordedFullName').val(data[0]);
+            $('#recordedPhoneNumber').val(data[2]);
+            $('#recordedMinDatepicker').val(data[3]);
+            $('#recordedDoctor').val(data[1]);
+            $('#recordedStartTime').val(data[5]);
+            $('#recordedEndTime').val(data[6]);
+            $('#recordedLament').val(data[4]);
+            $('#recordedCard').modal("show");
+            $('.record_for_delete').attr('data-id',record_id);
+        },
+        error: function (data) {
+            alert(123456);
+        }
+    })
+}
+
+function closeCard() {
+    $('.recorded_data_patient').val(' ');
+}
+function deleteCard(elem) {
+    $.ajax({
+        url: '../php/record_to_doctor/deleteRecordedCard.php',
+        method: 'POST',
+        data: {
+            fullName: $('#recordedFullName').val(),
+            date: $('#recordedMinDatepicker').val(),
+            time: $('#recordedStartTime').val(),
+        },
+        dataType: 'JSON',
+        success: function (data) {
+            console.log(elem);
+            let tr = document.getElementById(elem);
+            tr.remove();
+            alert("ggfgfg");
+            $('#recordedCard').modal("hide");
+        },
+        error: function (data) {
+            alert('BAD');
+        }
+    })
+}
+
+/*function insertData(patientName,lament,number,dateRecord,doctor,startTime,endTime) {
+
+}*/
+/*
 if ($("#payment").click){
     $("#paymentCard").modal("hide");
 }
@@ -192,3 +514,4 @@ if($("#plan").click){
     $("#recordingCard").modal("hide");
 
 }
+*/
