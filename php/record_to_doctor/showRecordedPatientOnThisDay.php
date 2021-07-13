fb0063afc5a454a412c12ef032be5db9ff3c0e32
Madihander <79453431+Madihander@users.noreply.github.com>
2021-07-13 10:32:21 +0600

    Update showRecordedPatientOnThisDay.php
    Теперь этот файл работает. Мой самый главный враг был побежден.

--- php/record_to_doctor/showRecordedPatientOnThisDay.php
+++ php/record_to_doctor/showRecordedPatientOnThisDay.php
@@ -1,8 +1,12 @@
 <?php
 require_once '../../includes/db.php';
 global $pdo;
-echo 123;
-//$sql = "SELECT * FROM patients.patients_record_data WHERE startTime = :startTime AND dateRecord  =:dateRecord";
-$stmt = $pdo->query('SELECT * FROM patients.patients_record_data');
-$row = $stmt->fetchAll();
-var_dump($row);
+$date = $_POST['date'] ?? null;
+$data = [];
+$sql = "SELECT * FROM record_patient WHERE dateRecord  =:dateRecord";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['dateRecord' => $date]);
+while ($row = $stmt->fetchall()) {
+    $data = $row;
+    echo json_encode($data);
+}
