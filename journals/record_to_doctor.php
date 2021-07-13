ea916744206dc088b1af11db6aa14d6e3132b1bd
Madihander <79453431+Madihander@users.noreply.github.com>
2021-07-13 10:21:59 +0600

    Update record_to_doctor.php
    Изменение дизайна страницы

--- journals/record_to_doctor.php
+++ journals/record_to_doctor.php
@@ -14,108 +14,89 @@ https://itchief.ru/bootstrap/modal-->
     <link rel="stylesheet" href="../css/jquery-ui.css">
 </head>
 <body>
-<div class="container-fluid pt-5">
-
-    <div class="col-12 pt-3 pb-3"></div>
-    <div class="row">
-        <div class="col-4 pr-3">
-            <div class="row">
-                <div class="col-1"></div>
-                <div class="col-10">
-                    <div id="datepicker"></div>
-                    <input type="hidden" id="datepicker_value" value="01.08.2019">
-                </div>
-                <div class="col-1"></div>
-            </div>
-            <div class="row mt-4 pr-3 pl-3">
-                <div class="col-1"></div>
-                <div class="col-10 day_indic">
-                    <p><big id="day_id"></big>
-                        <small id="month_id">.</small>
-                    </p>
-                    <p id="day_of_week"></p>
-                </div>
-                <div class="col-1"></div>
-            </div>
+<div class="container-fluid d-flex h-100 justify-content-center align-content-center align-items-center">
+    <div class="row glassEffect">
+        <div class="col-3 mt-4 mr-5">
+            <div id="datepicker" class="p-1"></div>
+            <input type="hidden" id="datepicker_value">
         </div>
-        <div class="col-7 pl-3 mr-3">
-            <table class="table table-hover table_patient">
+        <div class="col-7 mt-4">
+            <table id="table_patients" class="table table-hover table_patient">
                 <thead>
                 <tr>
-                    <th scope="col">Время</th>
-                    <th scope="col"><a href="#recordingCard" data-toggle="modal">Пациенты</a></th>
+                    <th scope="col">
+                        <span class=""><ion-icon name="time-outline"></ion-icon></span>
+                        <span class="">Время</span>
+                    </th>
+                    <th scope="col">
+                        <a class="p-1" href="#recordingCard" data-toggle="modal">
+                            <ion-icon width name="add-circle-outline"></ion-icon>
+                            Пациенты
+                        </a>
+                    </th>
+
                 </tr>
                 </thead>
-                <tbody id="tbody" class="connectedSortable">
-
+                <tbody id="tbody">
+                <tr class="tr_table_patient">
+                    <td><a href="#">10:00 - 11:00</a></td>
+                    <td>Кажибеков Мади Нуржанович</td>
+                </tr>
+                <tr class="tr_table_patient">
+                    <td><a href="#">10:00 - 11:00</a></td>
+                    <td>Кажибеков Мади Нуржанович</td>
+                </tr>
+                <tr class="tr_table_patient">
+                    <td><a href="#">10:00 - 11:00</a></td>
+                    <td>Кажибеков Мади Нуржанович</td>
+                </tr>
                 </tbody>
             </table>
         </div>
-        <div class="col-1"></div>
     </div>
 </div>
 <!--Модальное окно для того чтобы записать пациента-->
 <div id="recordingCard" class="modal fade">
     <div class="modal-dialog modal-dialog-centered modal-xl">
         <div class="modal-content">
-            <div class="modal-header pt-0 pb-0">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-1">
-                            <button><a id="recording" href="#recordingCard" data-toggle="modal">Запись</a></button>
-                        </div>
-                        <!--<div class="col-1">
-                            <button><a id="payment" href="#paymentCard" data-toggle="modal">Оплата</a></button>
-                        </div>-->
-                        <!--<div class="col-1">
-                            <button><a id="plan" href="#planCard" data-toggle="modal">План</a></button>
-                        </div>-->
-                        <div class="col-7"></div>
-                        <div class="col-1">
-                            <a href="patient_card.html">
-                                <img src="#" alt="record">
-                            </a>
-                        </div>
-                        <!--<div class="col-1">
-                            <a href="#">
-                                <img src="#" alt="123141">
-                            </a>
-                        </div>-->
-                    </div>
-                </div>
+            <div class="modal-header pt-2 pb-2">
+                <h3>Запись Пациента</h3>
             </div>
             <div class="modal-body mt-3">
                 <form id="form" class="container-fluid">
                     <div class="row mb-3">
-                        <div class="col-md-5">
-                            <label>ФИО Пациента</label>
-                            <input placeholder="Петров Петр Петрович" class="data_patient floating_input _req _fullName" id="fullName">
+                        <div class="col-5  mt-2">
+                            <label for="fullName" class="field_label">ФИО</label>
+                            <input id="fullName" class="fields" type="text" required autocomplete="off"
+                                   placeholder="Петров Петр Петрович">
                         </div>
-                        <div class="col-md-2"></div>
-                        <div class="col-md-5 ml-auto">
-                            <label>Номера телефона</label>
-                            <input class="data_patient floating_input _req _phoneNumber" type="text" id="phoneNumber">
-                        </div>
-                    </div>
-
-                    <div class="row mb-3">
-                        <div class="col-md-5">
-                            <label>День приема</label>
-                            <input class="data_patient floating_input _req _minDatepicker" readonly type="text"
-                                   id="minDatepicker">
-                            <!--<input class="floating_input" type="text" id="datepicker">-->
-                        </div>
-                        <div class="col-md-2"></div>
-                        <div class="col-md-5 ml-auto">
-                            <label>Лечущий Врач</label>
-                            <input class="data_patient floating_input _req _doctor" id="doctor">
+                        <div class="col-2"></div>
+                        <div class="col-5 mt-2">
+                            <label for="phoneNumber" class="field_label">Номера телефона</label>
+                            <input id="phoneNumber" class="fields" type="text" required autocomplete="off">
                         </div>
                     </div>
 
                     <div class="row mb-3">
+                        <div class="col-5">
+                            <label for="minDatepicker" class="field_label">Дата записи</label>
+                            <input id="minDatepicker" class="fields" type="text" required autocomplete="off">
+                        </div>
+                        <div class="col-2"></div>
+                        <div class="col-5">
+                            <label class="field_label" for="doctor">Лечущий врач</label>
+                            <select id="doctor" class="fields">
+                                <option>_____</option>
+                                <option value="Кажибеков Нуржан Султанович">Кажибеков Нуржан Султанович</option>
+                                <option value="Кажибеков Мади Нуржанович">Кажибеков Мади Нуржанович</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="row">
                         <div class="col-md-2">
-                            <label>Время начала</label>
-                            <select class="data_patient floating_input _startTimeHour" id="startTimeHour">
+                            <label for="startTimeHour">Время начала</label>
+                            <select class="selectionField _startTimeHour" id="startTimeHour">
                                 <option disabled value="">__</option>
                                 <option value="09">09</option>
                                 <option value="10">10</option>
@@ -130,7 +111,7 @@ https://itchief.ru/bootstrap/modal-->
                                 <option value="19">19</option>
                             </select>
                             :
-                            <select class="data_patient floating_input _startTimeMin" id="startTimeMin">
+                            <select class="selectionField  _startTimeMin" id="startTimeMin">
                                 <option disabled value="">__</option>
                                 <option value="00">00</option>
                                 <option value="05">05</option>
@@ -149,8 +130,8 @@ https://itchief.ru/bootstrap/modal-->
                         </div>
                         <div class="col-md-1"></div>
                         <div class="col-md-2">
-                            <label>Время окончания</label>
-                            <select onclick="hideOption()" class="data_patient floating_input _endTimeHour" id="endTimeHour">
+                            <label for="endTimeHour">Время окончания</label>
+                            <select onclick="hideOption()" class="selectionField _req _endTimeHour" id="endTimeHour">
                                 <option disabled value="">__</option>
                                 <option id="endHour09" value="09">09</option>
                                 <option id="endHour10" value="10">10</option>
@@ -165,7 +146,7 @@ https://itchief.ru/bootstrap/modal-->
                                 <option id="endHour19" value="19">19</option>
                             </select>
                             :
-                            <select class="data_patient floating_input _endTimeMin" id="endTimeMin">
+                            <select class="selectionField  _endTimeMin" id="endTimeMin">
                                 <option disabled value="">__</option>
                                 <option value="00">00</option>
                                 <option value="05">05</option>
@@ -182,16 +163,17 @@ https://itchief.ru/bootstrap/modal-->
                                 <option value="60">60</option>
                             </select>
                         </div>
+
                         <div class="col-2"></div>
+
                         <div class="col-md-5">
-                            <label>Причина обращения</label>
-                            <input class="data_patient floating_input _req _lament" id="lament">
+                            <label for="lament" class="field_label">Причина обращения</label>
+                            <input class="fields floating_input _req _lament" id="lament">
                         </div>
                     </div>
                 </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
                 <button type="button" onclick="formSend()" id="submit" class="btn btn-primary">Записать</button>
             </div>
         </div>
@@ -201,74 +183,51 @@ https://itchief.ru/bootstrap/modal-->
 <div id="recordedCard" class="modal fade">
     <div class="modal-dialog modal-dialog-centered modal-xl">
         <div class="modal-content">
-            <div class="modal-header pt-0 pb-0">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-1">
-                            <button><a id="recording" href="#recordingCard" data-toggle="modal">Запись</a></button>
-                        </div>
-                        <!--<div class="col-1">
-                            <button><a id="payment" href="#paymentCard" data-toggle="modal">Оплата</a></button>
-                        </div>-->
-                        <!--<div class="col-1">
-                            <button><a id="plan" href="#planCard" data-toggle="modal">План</a></button>
-                        </div>-->
-                        <div class="col-7"></div>
-                        <div class="col-1">
-                            <a href="patient_card.html">
-                                <img src="#" alt="record">
-                            </a>
-                        </div>
-                        <!--<div class="col-1">
-                            <a href="#">
-                                <img src="#" alt="123141">
-                            </a>
-                        </div>-->
-                    </div>
-                </div>
+            <div class="modal-header pt-2 pb-2">
+                <h3>Запись Пациента</h3>
             </div>
             <div class="modal-body mt-3">
                 <div class="container-fluid">
                     <div class="row mb-3">
                         <div class="col-md-5">
-                            <label>ФИО Пациента</label>
-                            <input class="recorded_data_patient floating_input" id="recordedFullName" readonly>
+                            <label class="field_label">ФИО Пациента</label>
+                            <input class="recorded_data_patient fields" id="recordedFullName" readonly>
                         </div>
                         <div class="col-md-2"></div>
                         <div class="col-md-5 ml-auto">
-                            <label>Номера телефона</label>
-                            <input class="recorded_data_patient floating_input" type="text" id="recordedPhoneNumber"
+                            <label class="field_label">Номера телефона</label>
+                            <input class="recorded_data_patient fields" type="text" id="recordedPhoneNumber"
                                    readonly>
                         </div>
                     </div>
 
                     <div class="row mb-3">
                         <div class="col-md-5">
-                            <label>День приема</label>
-                            <input class="recorded_data_patient" type="text" id="recordedMinDatepicker" readonly>
+                            <label class="field_label">День приема</label>
+                            <input class="recorded_data_patient fields" type="text" id="recordedMinDatepicker" readonly>
                             <!--<input class="floating_input" type="text" id="datepicker">-->
                         </div>
                         <div class="col-md-2"></div>
                         <div class="col-md-5 ml-auto">
-                            <label>Лечущий Врач</label>
-                            <input class="recorded_data_patient floating_input" id="recordedDoctor" readonly>
+                            <label class="field_label">Лечущий Врач</label>
+                            <input class="recorded_data_patient fields" id="recordedDoctor" readonly>
                         </div>
                     </div>
 
                     <div class="row mb-3">
                         <div class="col-md-2">
-                            <label>Время начала</label>
-                            <input class="recorded_data_patient floating_input" id="recordedStartTime" readonly>
+                            <label class="field_label">Время начала</label>
+                            <input class="recorded_data_patient fields" id="recordedStartTime" readonly>
                         </div>
                         <div class="col-md-1"></div>
                         <div class="col-md-2">
-                            <label>Время окончания</label>
-                            <input class="recorded_data_patient floating_input" id="recordedEndTime" readonly>
+                            <label class="field_label">Время окончания</label>
+                            <input class="recorded_data_patient fields" id="recordedEndTime" readonly>
                         </div>
                         <div class="col-2"></div>
                         <div class="col-md-5">
-                            <label>Жалоба</label>
-                            <input class="recorded_data_patient floating_input" id="recordedLament" readonly>
+                            <label class="field_label">Жалоба</label>
+                            <input class="recorded_data_patient fields" id="recordedLament" readonly>
                         </div>
                     </div>
                 </div>
@@ -278,7 +237,8 @@ https://itchief.ru/bootstrap/modal-->
                 <button id="closeRecordedCard" type="button" class="btn btn-default" data-dismiss="modal">
                     Закрыть
                 </button>
-                <button id="deleteRecordedCard" onclick="deleteCard($('.record_for_delete').attr('data-id'))" type="button" class="btn btn-default">
+                <button id="deleteRecordedCard" onclick="deleteCard($('.record_for_delete').attr('data-id'))"
+                        type="button" class="btn btn-default">
                     Удалить
                 </button>
 
@@ -290,10 +250,14 @@ https://itchief.ru/bootstrap/modal-->
 <script src="../jquery/jquery.js"></script>
 <script src="../js/bootstrap.js"></script>
 <script src="../js/popper.js"></script>
+<script src="../libs/tablesort.js"></script>
+
 <script src=../js/record_to_doctor.js></script>
 <!--<script src="https://code.jquery.com/jquery-1.12.4.js"></script>-->
 <script src="../jquery/jquery-ui.js"></script>
 <script src="../jquery/jquery.maskedinput.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 <script>
     /* Локализация datepicker */
     $.datepicker.regional['ru'] = {
@@ -315,6 +279,5 @@ https://itchief.ru/bootstrap/modal-->
     };
     $.datepicker.setDefaults($.datepicker.regional['ru']);
 </script>
-
 </body>
 </html>
