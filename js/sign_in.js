f5ca2cf69d8c2418f31f7606cc44c0660e1f202a
Madihander <79453431+Madihander@users.noreply.github.com>
2021-07-13 09:48:36 +0600

    Create sign_in.js
    sign.js существует для того чтобы получить введенные данные, а после отправить их с помощью ajax на проверку к файлу sign_in_handle.php

---
+++ js/sign_in.js
@@ -0,0 +1,47 @@
+$('#auth_btn').click(function (e) {
+    e.preventDefault();
+    let login = $('#login').val();
+    let pass = $('#password').val();
+    let error = verifyData(login, pass);
+    if (error !== 0) {
+        alert('Впишите данные в поля')
+        $('._req').css('border', '1px solid red');
+    } else {
+        $.ajax({
+            url: 'php/sign/sign_in_handle.php',
+            type: 'POST',
+            dataType: 'JSON',
+            data: {
+                login: login,
+                pass: pass,
+            },
+            before: function () {
+                alert("Запрос Отправлен");
+            },
+            success: function (data) {
+                if (data.status === '200' && data.error === '00') {
+                    alert("Запрос принят и успешно обработан");
+                    document.location.href="http://eljor.kz/journals/main_menu.php"
+                    console.log(data);
+                } else {
+                    alert("Запрос принят, но неудачно обработан");
+                    console.log(data);
+
+                }
+            },
+            error: function (data) {
+                console.log(data);
+                alert("ERROR.Запрос принят, но неудачно обработан");
+            }
+
+        });
+    }
+
+});
+
+function verifyData(login, pass) {
+    let error = 0
+    if (!login) error++;
+    if (!pass) error++
+    return error;
+}
