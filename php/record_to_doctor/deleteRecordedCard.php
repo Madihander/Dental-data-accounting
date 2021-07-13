eb23152ae6b5fc0271d11c84bad9695009b7500b
Madihander <79453431+Madihander@users.noreply.github.com>
2021-07-13 10:23:29 +0600

    Update deleteRecordedCard.php
    Карточка теперь будет удаляться только с помощью её id

--- php/record_to_doctor/deleteRecordedCard.php
+++ php/record_to_doctor/deleteRecordedCard.php
@@ -1,13 +1,11 @@
 <?php
 require_once '../../includes/db.php';
-$fullName = $_POST['fullName'];
-$date = $_POST['date'];
-$time = $_POST['time'];
+$id = $_POST['id'];
 
 global $pdo;
 $response = [];
 //    $sql = "SELECT * FROM patients.patients_record_data WHERE startTime = :startTime AND dateRecord  =:dateRecord";
-$stmt = $pdo->prepare('DELETE FROM patients.patients_record_data WHERE fullName=:fullName AND startTime =:startTime AND dateRecord  =:dateRecord');
-$stmt->execute(['fullName'=>$fullName,'startTime' => $time, 'dateRecord' => $date]);
-array_push($response,'good');
+$stmt = $pdo->prepare('DELETE FROM record_patient WHERE id =:id');
+$stmt->execute(['id' => $id]);
+array_push($response, 'good');
 echo json_encode($response);
